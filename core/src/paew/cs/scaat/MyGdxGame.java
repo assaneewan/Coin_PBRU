64cec66a0ca1d610436c1806c2ee8485f401177f
ASSANEEWAN <ASSANEEWAN@HOTMAIL.COM>
2015-08-21 17:44:16 +0700

    master test

--- core/src/paew/cs/scaat/MyGdxGame.java
+++ core/src/paew/cs/scaat/MyGdxGame.java
@@ -23,20 +23,23 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	private Texture wallpaperTexture, cloudTexture, atomRTexture, atomLTexture;
-	private Texture pigTexture, coinTexture, atomTexture;
+	private Texture pigTexture, coinTexture, atomTexture, rainTexture;
 	private OrthographicCamera objOrthographicCamera;
-	private BitmapFont nameBitmapFont;
+	private BitmapFont nameBitmapFont, scoreBitmapFont, showScoreBitmapFont;
 	private int xCloudAnInt,yCloudAnInt=580;
 	private int xAtomRAnInt,yAtomRAnInt = 580;
 	private int xAtomLAnInt, yAtomLAnInt = 1204;
 	private boolean atomRABoolean, atomLABoolean = true;
-	private boolean cloudABoolean = true;
-	private Rectangle pigRectangle, coinRectangle;
+	private boolean cloudABoolean = true, finishABoolean = false;
+	private Rectangle pigRectangle, coinRectangle, rainRectangle;
 	private Vector3 objVector3;
 	private Sound pigSound, waterDropSound, coinDropSound;
-	private Array<Rectangle> coinsArray;
-	private long lastDropCoins;
-	private Iterator<Rectangle> coinsIterator; // Java util
+	private Array<Rectangle> coinsArray, rainArray;
+	private long lastDropCoins, lastDropRain;
+	//วนหลายรูป
+	private Iterator<Rectangle> coinsIterator, rainIterator; // Java util
+	private int scoreAnInt = 0, falseAnInt = 0, finalScoreAnInt;
+	private Music rainMusic, bgMusic; // เสียงเป็น background loop
 
 
 	@Override
@@ -49,15 +52,25 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		wallpaperTexture = new Texture("ww3.png");
 
+		// ตัวอักษร
 		nameBitmapFont = new BitmapFont();
 		nameBitmapFont.setColor(Color.BLUE);
 		nameBitmapFont.scale(3);
+		scoreBitmapFont = new BitmapFont();
+		scoreBitmapFont.setColor(Color.OLIVE);
+		scoreBitmapFont.scale(3);
+		showScoreBitmapFont = new BitmapFont();
+		showScoreBitmapFont.setColor(230, 28, 223, 255);
+		showScoreBitmapFont.scale(6);
+
+
 
 		//cloudTexture = new Texture("p3_right.png");
 		atomTexture = new Texture("p3_right.png");
 		atomRTexture = new Texture("p3_right.png");
 		atomLTexture = new Texture("p3_left.png");
 		pigTexture = new Texture("pig.png");
+
 
 
 		//set up Rectangle Pig
@@ -67,6 +80,10 @@ public class MyGdxGame extends ApplicationAdapter {
 		pigRectangle.width = 64;
 		pigRectangle.height = 64;
 
+		// set up background sound
+		bgMusic = Gdx.audio.newMusic(Gdx.files.internal("bggame.mp3"));
+		// set up rain music
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 		//set up pig sound
 		pigSound = Gdx.audio.newSound(Gdx.files.internal("pig.wav"));
 		// set up water drop
@@ -77,11 +94,29 @@ public class MyGdxGame extends ApplicationAdapter {
 		// set up Coins
 		coinTexture = new Texture("coins.png");
 
+		// set up rain
+		rainTexture = new Texture("droplet.png");
+
 		// Create coinsArray
 		coinsArray = new Array<Rectangle>();
 		coinsRandomDrop();
 
+		// Create rainsArray
+		rainArray = new Array<Rectangle>();
+		rainRandomDrop();
+
 	} // create เอาไว้กำหนดค่า
+
+	private void rainRandomDrop() {
+		rainRectangle = new Rectangle();
+		rainRectangle.x = MathUtils.random(0, 1204);
+		rainRectangle.y = 800;
+		rainRectangle.width = 64;
+		rainRectangle.height = 64;
+
+		rainArray.add(rainRectangle);
+		lastDropRain = TimeUtils.nanoTime();
+	}
 
 	private void coinsRandomDrop() {
 
@@ -121,7 +156,20 @@ public class MyGdxGame extends ApplicationAdapter {
 		//drawable coins
 		for (Rectangle forCoins : coinsArray) {
 			batch.draw(coinTexture,forCoins.x,forCoins.y);
+		} // coin
+
+		// drawable rain
+		for (Rectangle forRain : rainArray) {
+			batch.draw(rainTexture,forRain.x,forRain.y);
+		} // rain
+
+		// drawable score
+		scoreBitmapFont.draw(batch, "Score = " + Integer.toString(scoreAnInt), 900, 750);
+		if (finishABoolean) {
+			batch.draw(wallpaperTexture,0,0);
+			showScoreBitmapFont.draw(batch, "Your Score = " + Integer.toString(finalScoreAnInt),300,400);
 		}
+
 		batch.end();
 		//move cloud
 		//moveCloud();
@@ -131,6 +179,40 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		// Random Drop Coins
 		randomDropCoins();
+
+		// Random drop Rain
+		randomDropRain();
+
+		// play music
+		rainMusic.play();
+
+		//backgound music
+		bgMusic.play();
+
+	} // render
+
+	private void randomDropRain() {
+		if (TimeUtils.nanoTime()-lastDropRain> 1E9) {
+			rainRandomDrop();
+		}
+
+		rainIterator = rainArray.iterator();
+		while (rainIterator.hasNext()) {
+			Rectangle myRainRectangle = rainIterator.next();
+			myRainRectangle.y -= 50 * Gdx.graphics.getDeltaTime();
+
+			// when rain drop into floor
+			if (myRainRectangle.y + 64 < 0) {
+				waterDropSound.play();
+				rainIterator.remove();
+			} // if
+			if (myRainRectangle.overlaps(pigRectangle)) {
+				waterDropSound.play();
+				scoreAnInt -= 1;
+				rainIterator.remove();
+			}
+		}// while
+
 	}
 
 	private void randomDropCoins() {
@@ -146,20 +228,46 @@ public class MyGdxGame extends ApplicationAdapter {
 
 			//When Coins into Floor
 			if (myCoinsRectangle.y + 64 < 0) {
+				falseAnInt += 1;
 				waterDropSound.play();
 				coinsIterator.remove(); // เหรียญหายไปคืนหน่วยความจำให้ระบบ
+				checkFalse();
 
 			} // if
 			// When Coins Overlap Pig
 			// การ overlaps บางส่วนของภาพไปโดนบางส่วนของอีกภาพ
 			if (myCoinsRectangle.overlaps(pigRectangle)) {
 				coinDropSound.play();
+				scoreAnInt += 1;
 				coinsIterator.remove();
 			} // if
 
 		} // while loop
 
 	} //randomDropCoins
+
+	private void checkFalse() {
+		if (falseAnInt > 20) {
+
+			dispose();
+			if (!finishABoolean) {
+				finalScoreAnInt = scoreAnInt;
+			}
+			finishABoolean = true;
+
+		}
+	} // check False
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		bgMusic.dispose();
+		rainMusic.dispose();
+		coinDropSound.dispose();
+		pigSound.dispose();
+		waterDropSound.dispose();
+
+	} // dispose
 
 	private void activeTouchScreen() {
 		// นิ้วสัมผัสจอ
